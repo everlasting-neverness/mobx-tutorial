664ef3669bfb8a2f89241cb504e3ba21962616c9
Vasily <gerasimovv1990@gmail.com>
2020-04-05 16:42:51 +0300

    update

--- src/App.js
+++ src/App.js
@@ -1,35 +1,50 @@
 import React from 'react';
 import Counter from './components/Counter';
-import { observable } from 'mobx';
+import { observable, configure, action } from 'mobx';
 
-const nickName = observable({
-  firstName: 'John',
-  age: 30,
-  get nickName() {
-    console.log('Generate nick');
-    return `${this.firstName}${this.age}`;
-  },
-  increment() { this.age++ },
-  decrement() { this.age-- },
-});
+configure({ enforceActions: 'observed' });
 
-const todos = observable([
-  {
-    text: 'Learn React',
-  },
-  {
-    text: 'Learn MobX',
-  },
-]);
+const nickName = observable(
+    {
+        firstName: 'John',
+        age: 30,
+        get nickName() {
+            console.log('Generate nick');
+            return `${this.firstName}${this.age}`;
+        },
+        increment() {
+            this.age++;
+        },
+        decrement() {
+            this.age--;
+        },
+    },
+    {
+        increment: action('Plus one'),
+        decrement: action('Minus one'),
+    },
+    {
+      name: 'nickNameObservableObject'
+    }
+);
+
+// const todos = observable([
+//   {
+//     text: 'Learn React',
+//   },
+//   {
+//     text: 'Learn MobX',
+//   },
+// ]);
 
 function App() {
-  return (
-    <div className="App">
-      <Counter store={todos} />
-    </div>
-  );
+    return (
+        <div className='App'>
+            <Counter store={nickName} />
+        </div>
+    );
 }
 
 export default App;
 
-todos.push({ text: 'Learn Redux' });
+// todos.push({ text: 'Learn Redux' });
--- src/components/Counter.js
+++ src/components/Counter.js
@@ -3,21 +3,21 @@ import { observer } from 'mobx-react';
 
 @observer class Counter extends Component {
 
-    // handleIncrement = () => { this.props.store.increment() };
-    // handleDecrement = () => { this.props.store.decrement() };
+    handleIncrement = () => { this.props.store.increment() };
+    handleDecrement = () => { this.props.store.decrement() };
 
     render() {
         console.log(this.props)
         return (
             <div>
-                {/* <h1>{this.props.store.age}</h1>
+                <h1>{this.props.store.age}</h1>
                 <h2>{this.props.store.nickName}</h2>
                 <button onClick={this.handleDecrement}>-1</button>
                 <button onClick={this.handleIncrement}>+1</button>
-                <br/> */}
-                <ul>
+                <br/>
+                {/* <ul>
                     {this.props.store.map(({ text }) => <li key={text}>{text}</li>)}
-                </ul>
+                </ul> */}
             </div>
         )
     }
