e3911e78be42b8eeec634376ddff90925e746b6b
Vasily <gerasimovv1990@gmail.com>
2020-04-05 17:13:21 +0300

    update

--- src/index.js
+++ src/index.js
@@ -2,11 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import StoryPointsApp, { Store } from './components/StoryPointsApp';
 import * as serviceWorker from './serviceWorker';
+
+const storePointsAppStore = new Store();
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <StoryPointsApp store={storePointsAppStore} />
   </React.StrictMode>,
   document.getElementById('root')
 );
